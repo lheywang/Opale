382aefb032a972efcd8e33d3739cbd98e2f24f84
lheywang <leonard.heywang@gmail.com>
2025-02-17 16:45:48 +0100

    Tried some things to use the DTS

--- src/main.c
+++ src/main.c
@@ -1,15 +1,33 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
+#include <zephyr/drivers/gpio.h>
 
-#ifdef CONFIG_GPIOHANDLE
-    #include "peripherals/gpio.h"
-#endif
+#define LED0_NODE DT_ALIAS(led0) // LED0_NODE = led0 defined in the .dts file
+
+static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED0_NODE, gpios);
 
 int main(void)
 {
+
+    if (!gpio_is_ready_dt(&led)) {
+		return 0;
+	}
+
+    int ret;
+
+    ret = gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE);
+    if (ret < 0) {
+        return 0;
+    }
+
 	while (1) {
 		printk("Hello World!\n");
 		k_msleep(1000);
+
+        ret = gpio_pin_toggle_dt(&led);
+        if (ret < 0) {
+            return 0 ;
+        }
 	}
 }
 
