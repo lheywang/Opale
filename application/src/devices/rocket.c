20340d15f8ae5d03387df4ace83d649aee81ddb4
lheywang <leonard.heywang@gmail.com>
2025-02-23 21:49:36 +0100

    Added SAADC drivers files

---
+++ application/src/devices/rocket.c
@@ -0,0 +1,62 @@
+/** ================================================================
+ * @file    application/src/devices/rocket.c
+ *
+ * @brief   rocket.c implement low level rocket functions, mainly
+ *          for the launch authorization.
+ *
+ * @date    23-02-2025
+ *
+ * @version 1.0.0
+ * 
+ * @author  l.heywang (leonard.heywang@proton.me)
+ * 
+ *  ================================================================
+ */
+
+/* -----------------------------------------------------------------
+* INCLUDING LIBS
+* -----------------------------------------------------------------
+*/
+
+// Zephyr
+#include <zephyr/logging/log.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/kernel.h>
+
+// Local libs
+#include "rocket.h"
+#include "gpio.h"
+#include "../config.h"
+
+/* -----------------------------------------------------------------
+* LOGGER CONFIG
+* -----------------------------------------------------------------
+*/
+// Identify the module on the LOG Output
+LOG_MODULE_REGISTER(Rocket, PROJECT_LOG_LEVEL);
+
+/* -----------------------------------------------------------------
+* FUNCTIONS TO FETCH THE MODE
+* -----------------------------------------------------------------
+*/
+
+int ROCKET_GetOperationMode(const struct gpio_dt_spec *Latch, 
+                            const struct gpio_dt_spec *Status, 
+                            OperationMode *const Mode){
+
+        // First, trigger the GPIO latching process
+        if (!GPIO_Set(Latch, 1));
+            return 1;
+        k_msleep(5);
+        if(!GPIO_Set(Latch, 0));
+            return -1;
+
+        // Then, read back the value
+        k_msleep(5);
+        int tmp = 0;
+        GPIO_Read(Status, &tmp);
+
+        // Copy the data back.
+        OperationMode *Mode = tmp;
+        return 0;
+    }
