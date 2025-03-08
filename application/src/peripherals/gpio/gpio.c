3edd7489d30047c3dfbaee41e2a87a19f1309bd7
lheywang <leonard.heywang@gmail.com>
2025-03-08 16:21:05 +0100

    Moved some peripherals drivers files to a dedicated folder

---
+++ application/src/peripherals/gpio/gpio.c
@@ -0,0 +1,64 @@
+/** ================================================================
+ * @file    application/src/peripherals/gpio/gpio.c
+ *
+ * @brief   rgb.c implement some higher level functions to control 
+ *          GPIO
+ *
+ * @date    20-02-2025
+ *
+ * @version 1.0.0
+ * 
+ * @author  l.heywang (leonard.heywang@proton.me)
+ * 
+ *  ================================================================
+ */
+
+/* -----------------------------------------------------------------
+* INCLUDING LIBS
+* -----------------------------------------------------------------
+*/
+
+// Zephyr
+#include <zephyr/drivers/gpio.h>
+
+// Local libs
+#include "gpio.h"
+
+/* -----------------------------------------------------------------
+* FUNCTIONS TO COMMAND A SERVO
+* -----------------------------------------------------------------
+*/
+
+
+int GPIO_SetAsInput(const struct gpio_dt_spec *Target){
+    return gpio_pin_configure_dt(Target, GPIO_INPUT);
+}
+
+
+int GPIO_SetAsOutput(const struct gpio_dt_spec *Target, const uint8_t State){
+    if (State){
+        return gpio_pin_configure_dt(Target, GPIO_OUTPUT_ACTIVE);
+    }
+    return gpio_pin_configure_dt(Target, GPIO_OUTPUT_INACTIVE);
+}
+
+
+int GPIO_SetAsHighZ(const struct gpio_dt_spec *Target){
+    return gpio_pin_configure_dt(Target, GPIO_DISCONNECTED);
+}
+
+
+int GPIO_Toggle(const struct gpio_dt_spec *Target){
+    return gpio_pin_toggle_dt(Target);
+}
+
+
+int GPIO_Set(const struct gpio_dt_spec *Target, const int Value){
+    return gpio_pin_set_dt(Target, Value);
+}
+
+
+int GPIO_Read(const struct gpio_dt_spec *Target, int *const Value){
+    *Value = gpio_pin_get_dt(Target);
+    return 0;
+}
---
+++ application/src/peripherals/gpio/gpio.h
@@ -0,0 +1,115 @@
+/** ================================================================
+ * @file    application/src/peripherals/gpio/gpio.h
+ *
+ * @brief   rgb.h define some higher level interractions with the
+ *          base GPIO which can be quite complicated.
+ *          
+ *
+ * @date    20-02-2025
+ *
+ * @version 1.0.0
+ * 
+ * @author  l.heywang (leonard.heywang@proton.me)
+ * 
+ *  ================================================================
+ */
+
+// Include once
+#ifndef DEF_GPIO
+    #define DEF_GPIO
+
+    /* -----------------------------------------------------------------
+    * INCLUDING LIBS
+    * -----------------------------------------------------------------
+    */
+
+    // Zephyr
+    #include <zephyr/drivers/gpio.h>
+
+    /* -----------------------------------------------------------------
+    * FUNCTIONS TO COMMAND A GPIO
+    * -----------------------------------------------------------------
+    */
+
+    /**
+     * @brief   Set a defined GPIO as input.
+     * 
+     * @warning This function take account of hardware inversion of pins,
+     *          that may been done in the device tree.
+     * 
+     * @param   Target  The GPIO to be configured
+     * 
+     * @return  0 Pin configured
+     * @return !0 Error
+     */
+    int GPIO_SetAsInput(const struct gpio_dt_spec *Target);
+
+    /**
+     * @brief   Set a defined GPIO as output.
+     * 
+     * @warning This function take account of hardware inversion of pins,
+     *          that may been done in the device tree.
+     * 
+     * @param   Target  The GPIO to be configured
+     * @param   State   The state taken by the GPIO right after
+     * 
+     * @return  0 Pin configured
+     * @return !0 Error
+     */
+    int GPIO_SetAsOutput(const struct gpio_dt_spec *Target, const uint8_t State);
+
+    /**
+     * @brief 
+     * 
+     * @warning This function take account of hardware inversion of pins,
+     *          that may been done in the device tree.
+     * 
+     * @param   Target  The GPIO to be configured
+     * 
+     * @return  0 Pin configured
+     * @return !0 Error
+     */
+    int GPIO_SetAsHighZ(const struct gpio_dt_spec *Target);
+
+    /**
+     * @brief 
+     * 
+     * @warning This function take account of hardware inversion of pins,
+     *          that may been done in the device tree.
+     * 
+     * @param   Target  The GPIO to be toggled.
+     * 
+     * @return  0 Pin configured
+     * @return !0 Error
+     */
+    int GPIO_Toggle(const struct gpio_dt_spec *Target);
+
+    /**
+     * @brief 
+     * 
+     * @warning This function take account of hardware inversion of pins,
+     *          that may been done in the device tree.
+     * 
+     * @param   Target  The GPIO to be set
+     * @param   Value   The value to be wrote on the GPIO
+     * 
+     * @return  0 Pin configured
+     * @return !0 Error
+     */
+    int GPIO_Set(const struct gpio_dt_spec *Target, const int Value);
+
+    /**
+     * @brief 
+     * 
+     * @warning This function take account of hardware inversion of pins,
+     *          that may been done in the device tree.
+     * 
+     * @param   Target  The GPIO to be read
+     * @param   Value   Pointer to an integer to be filled.
+     * 
+     * @return  0 Always 0.
+     */
+    int GPIO_Read(const struct gpio_dt_spec *Target, int *const Value);
+
+#endif
+
