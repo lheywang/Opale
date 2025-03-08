e6c56c9e2c2dd52215af893a5ed6581c675d340d
lheywang <leonard.heywang@gmail.com>
2025-03-08 21:55:31 +0100

    Used a library for the MCP23008 as the driver. Will need some work on it!

--- application/src/drivers/mcp23008/Project Example/STM32L434KCU6/Debug/Core/mcp23008_driver/mcp23008_driver.d
+++
@@ -1,4 +0,0 @@
-Core/mcp23008_driver/mcp23008_driver.o: \
- ../Core/mcp23008_driver/mcp23008_driver.c \
- ../Core/mcp23008_driver/mcp23008_driver.h
-../Core/mcp23008_driver/mcp23008_driver.h:
--- application/src/drivers/mcp23008/Project Example/STM32L434KCU6/Debug/Core/mcp23008_driver/mcp23008_driver_basic.d
+++
@@ -1,8 +0,0 @@
-Core/mcp23008_driver/mcp23008_driver_basic.o: \
- ../Core/mcp23008_driver/mcp23008_driver_basic.c \
- ../Core/mcp23008_driver/mcp23008_driver_basic.h \
- ../Core/mcp23008_driver/mcp23008_driver_interface.h \
- ../Core/mcp23008_driver/mcp23008_driver.h
-../Core/mcp23008_driver/mcp23008_driver_basic.h:
-../Core/mcp23008_driver/mcp23008_driver_interface.h:
-../Core/mcp23008_driver/mcp23008_driver.h:
