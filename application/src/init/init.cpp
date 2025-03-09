a8f24a696ac7f54ffefe10b11ae2b028abcffb5a
lheywang <leonard.heywang@gmail.com>
2025-03-09 17:12:53 +0100

    Refactored Initializer to use dynamic memory rather than globally defined variables. Much cleaner, but need some work before been buildiable !

--- application/src/init/init.cpp
+++ application/src/init/init.cpp
@@ -1,12 +1,12 @@
 /** ================================================================
  * @file    application/src/init/init.cpp
  *
- * @brief   Init.c define the function that check if the peripherals
+ * @brief   Init.cpp define the function that check if the peripherals
  *          are ready to be used, or not.
  *
- * @date    19-02-2025
+ * @date    09-03-2025
  *
- * @version 1.0.0
+ * @version 2.0.0
  *
  * @author  l.heywang (leonard.heywang@proton.me)
  *
--- application/src/init/init.h
+++ application/src/init/init.h
@@ -6,9 +6,9 @@
  *          This file also do some verification about the fetched
  *          structure to ensure they're usable.
  *
- * @date    19-02-2025
+ * @date    09-03-2025
  *
- * @version 1.0.0
+ * @version 2.0.0
  *
  * @author  l.heywang (leonard.heywang@proton.me)
  *
