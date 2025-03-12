928a34e702c9ad1d8669c1e967f9a3be01b04383
lheywang <leonard.heywang@gmail.com>
2025-03-12 16:09:39 +0100

    Implemented the MS5611 driver

--- application/src/drivers/MS5611/MS5611.cpp
+++ application/src/drivers/MS5611/MS5611.cpp
@@ -3,105 +3,149 @@ MS5611.h
 Library for barometric pressure sensor MS5611-01BA on I2C with arduino
 
 by Petr Gronat@2014
-*/
-#define OSR 2 // 0-3
-#define CMD_RESET 0x1E
-#define CMD_ADC_READ 0x00
-#define CMD_CONV_D1_BASE 0x40
-#define CMD_CONV_D2_BASE 0x50
-#define CONV_REG_SIZE 0x02
-#define CMD_PROM_READ_BASE 0xA2
-#define PROM_REG_SIZE 0x02
-#define NBYTES_CONV 3
-#define NBYTES_PROM 2
 
-// Temperature sampling period threshold [milliseconds]
-// Kindly read the comment bellow in getPressure() method
-#define T_THR 1000
-/*
-TODO:
-1) Separate OSR for temperature and pressure
-2) Timedelay empirical formula for temperature oversampling
-3) Precidion pressure celibration for high temperature
-4) Default and optional OSR
-5) Documentation
+Edited by l.heywang in 2025 to make it compatible with nRF (Zephyr) API.
 */
 
+// Libs
 #include "MS5611.h"
 #include <cstdint>
 
+// Zephyr
+#include <zephyr/drivers/i2c.h>
+#include <zephyr/kernel.h>
+
+// init lib
+#include "../../init/init.h"
+
+constexpr uint8_t OSR = 3; // 0-3
+constexpr uint8_t CMD_RESET = 0x1E;
+constexpr uint8_t CMD_ADC_READ = 0x00;
+constexpr uint8_t CMD_CONV_D1_BASE = 0x40;
+constexpr uint8_t CMD_CONV_D2_BASE = 0x50;
+constexpr uint8_t CONV_REG_SIZE = 0x02;
+constexpr uint8_t CMD_PROM_READ_BASE = 0xA2;
+constexpr uint8_t PROM_REG_SIZE = 0x02;
+constexpr uint8_t NBYTES_CONV = 3;
+constexpr uint8_t NBYTES_PROM = 2;
+
+// Temperature sampling period threshold [milliseconds]
+// Kindly read the comment bellow in getPressure() method
+constexpr int T_THR = 1000;
+
 MS5611::MS5611()
 {
+    // Defining some settings
     _T = 0;
     _P = 0;
     _lastTime = T_THR;
+
     for (uint8_t k = 0; k < N_PROM_PARAMS; k++)
         _C[k] = 69;
+
+    // Fetch the associated device on the DT
+    i2c_dt_spec *dev DEV_STRUCT = INIT_GetAnI2C(I2CS::BAROMETER);
+
+    begin();
+}
+
+MS5611::~MS5611()
+{
+    // Close the I2C device
+    INIT_FreeAnI2C(I2CS::BAROMETER, dev);
+
+    // Variables are handled by the C++ compiler
 }
 
 void MS5611::begin()
 {
-    // TODO Replace here with custom I2C nRF API !
-    // Wire.begin();
     reset();
-
-    // TODO Replace here with custom Zephyr API call
-    // delay(100);
+    k_msleep(200);
     readCalibration();
 }
 
-int32_t MS5611::getPressure()
+double *MS5611::getPressure()
 {
-    getTemperature(); // updates temperature _dT and _T
+    double temp = getTemperature(); // updates offset values !
     uint32_t D1 = getRawPressure();
 
-    int64_t OFF = (int64_t)_C[2 - 1] * 65536 + (int64_t)_C[4 - 1] * _dT / 128;
+    // Datasheet P8 to understand what's done here...
+    int64_t OFF = (this->_C[2 - 1] * (2 ^ 16)) +
+                  (this->_C[4 - 1] * (this->_dT / (2 ^ 7))) -
+                  this->_Off2;
 
-    int64_t SENS = (int64_t)_C[1 - 1] * 32768 + (int64_t)_C[3 - 1] * _dT / 256;
-    _P = (D1 * SENS / 2097152 - OFF) / 32768;
-    return _P;
+    int64_t SENS = (this->_C[1 - 1] * (2 ^ 15)) +
+                   (this->_C[3 - 1] * (this->_dT / (2 ^ 8))) -
+                   this->_Sens2;
+
+    this->_P = D1 * SENS - OFF;
+    double *retval = (double *)k_malloc(2 * sizeof(double));
+    retval[0] = this->_P / 100;
+    retval[1] = temp;
+    return retval;
 }
 
 uint32_t MS5611::getRawPressure()
 {
     sendCommand(CMD_CONV_D1_BASE + OSR * CONV_REG_SIZE); // read sensor, prepare a data
-    // TODO delay(1 + 2 * OSR);                                  // wait at least 8.33us for full oversampling
-    sendCommand(CMD_ADC_READ);      // get ready for reading the data
-    return readnBytes(NBYTES_CONV); // reading the data
+    k_msleep(9);
+    return readnBytes(&CMD_ADC_READ, 1, NBYTES_CONV); // reading the data
 }
 
-int32_t MS5611::getTemperature()
+double MS5611::getTemperature()
 {
-    // Code below can be uncommented for slight speedup:
-    // NOTE: Be sure what you do! Notice that Delta 1C ~= Delta 2hPa
-    //****************
-    // if(abs(millis()-_lastTime)<T_THR)
-    // 	return _T;
-    //_lastTime = millis();
-    //****************
-    uint32_t D2;
-    D2 = getRawTemperature();
-    _dT = D2 - ((uint32_t)_C[5 - 1] * 256); // update '_dT'
-    // Below, 'dT' and '_C[6-1]'' must be casted in order to prevent overflow
-    // A bitwise division can not be dobe since it is unpredictible for signed integers
-    _T = 2000 + ((int64_t)_dT * _C[6 - 1]) / 8388608;
-    return _T;
+    uint32_t D2 = getRawTemperature();
+
+    // As per the datasheet, calculate the values
+    this->_dT = D2 - (this->_C[5 - 1] * (2 ^ 8));
+
+    this->_T = 2000 +
+               (this->_dT * (this->_C[6 - 1] / (2 ^ 23)));
+
+    int64_t T2 = 0;
+    int64_t OFF2 = 0;
+    int64_t SENS2 = 0;
+
+    // Datasheet P8 to understand what's done here...
+    if (this->_T < 20)
+    {
+        T2 = this->_dT / (2 ^ 31);
+
+        OFF2 = 5 * (((this->_T - 2000) ^ 2) / 2);
+
+        SENS2 = OFF2 / 2;
+
+        if (this->_T < -15)
+        {
+            OFF2 = OFF2 +
+                   (7 * ((this->_T + 1500) ^ 2));
+
+            SENS2 = SENS2 +
+                    (11 * (((this->_T + 1500) ^ 2) / 2));
+        }
+    }
+
+    // Store the values
+    this->_T = (this->_T - T2) / 100;
+    this->_Off2 = OFF2;
+    this->_Sens2 = SENS2;
+
+    return (this->_T - T2) / 100;
 }
 
 uint32_t MS5611::getRawTemperature()
 {
     sendCommand(CMD_CONV_D2_BASE + OSR * CONV_REG_SIZE); // read sensor, prepare a data
-    // TODO delay(1 + 2 * OSR);                                  // wait at least 8.33us
-    sendCommand(CMD_ADC_READ);      // get ready for reading the data
-    return readnBytes(NBYTES_CONV); // reading the data
+    k_msleep(9);
+    return readnBytes(&CMD_ADC_READ, 1, NBYTES_CONV);
 }
 
 void MS5611::readCalibration()
 {
     for (uint8_t k = 0; k < 6; k++)
     {
-        sendCommand(CMD_PROM_READ_BASE + k * 2);
-        _C[k] = (uint16_t)(readnBytes(NBYTES_PROM) & 0xFFFF); // masking out two LSB
+        uint8_t cmd = CMD_PROM_READ_BASE + k * 2;
+        _C[k] = (uint16_t)(readnBytes(&cmd, 1, NBYTES_PROM) & 0xFFFF); // masking out two LSB
     }
 }
 
@@ -114,37 +158,34 @@ void MS5611::getCalibration(uint16_t *C)
 
 void MS5611::sendCommand(uint8_t cmd)
 {
-    // TODO Replace here with custom I2C nRF API !
-    // Wire.beginTransmission(ADD_MS5611);
-    // Wire.write(cmd);
-    // Wire.endTransmission();
+    // Replaced with the zephyr call
+    i2c_write_dt(this->dev, &cmd, sizeof(cmd));
+    return;
 }
 
-uint32_t MS5611::readnBytes(uint8_t nBytes)
+uint32_t MS5611::readnBytes(const uint8_t *cmd, uint8_t wlen, uint8_t rlen)
 {
-    if (0 < nBytes & nBytes < 5)
-    {
-        // TODO Replace here with custom I2C nRF API !
-        // Wire.beginTransmission(ADD_MS5611);
-        // Wire.requestFrom((uint8_t)ADD_MS5611, nBytes);
-        uint32_t data = 0;
-        // if (Wire.available() != nBytes)
-        // {
-        //     Wire.endTransmission();
-        //     return NULL;
-        // }
-        // for (int8_t k = nBytes - 1; k >= 0; k--)
-        //     data |= ((uint32_t)Wire.read() << (8 * k)); // concantenate bytes
-        // Wire.endTransmission();
-        return data;
-    } // too many bytes or
-    return 0; // no byte required
+    if (!(0 < rlen) & (rlen < 5))
+        return -1;
+
+    // Allocate and clear the output buffer
+    int rbuf[4];
+    memset(rbuf, 0, rlen);
+
+    // Read bytes
+    *rbuf = i2c_write_read_dt(this->dev, cmd, wlen, rbuf, rlen);
+
+    int data = 0;
+    for (int8_t k = rlen - 1; k >= 0; k--)
+        data |= ((uint32_t)rbuf[sizeof(rlen) - k] << (8 * k)); // concantenate bytes
+
+    return data;
 }
 
 void MS5611::reset()
 {
-    // TODO Replace here with custom I2C nRF API !
-    // Wire.beginTransmission(ADD_MS5611);
-    // Wire.write(CMD_RESET);
-    // Wire.endTransmission();
+    // Replaced with the zephyr call
+    const uint8_t buf = CMD_RESET;
+    i2c_write_dt(this->dev, &buf, 1);
+    return;
 }
--- application/src/drivers/MS5611/MS5611.h
+++ application/src/drivers/MS5611/MS5611.h
@@ -6,6 +6,7 @@ by Petr Gronat@2014
 */
 
 #include <cstdint>
+#include <zephyr/drivers/i2c.h>
 
 // Include guard token - prevents to include header file twice
 #ifndef MS5611_h
@@ -20,25 +21,38 @@ by Petr Gronat@2014
 class MS5611
 {
 public:
-    MS5611(); // constructor
+    /*
+     * CONSTRUCTORS AND DESTRUCTORS
+     */
+    MS5611();  // constructor
+    ~MS5611(); // destructor
+
+    /*
+     * FUNCTIONS
+     */
     void begin();
     uint32_t getRawTemperature();
-    int32_t getTemperature();
+    double getTemperature();
     uint32_t getRawPressure();
-    int32_t getPressure();
+    double *getPressure();
     void readCalibration();
     void getCalibration(uint16_t *);
     void sendCommand(uint8_t);
-    uint32_t readnBytes(uint8_t);
+    uint32_t readnBytes(const uint8_t *, uint8_t, uint8_t);
 
 private:
     void reset();
+
     // variables
     int32_t _P;
     int32_t _T;
     int32_t _dT;
+    int64_t _Off2;
+    int64_t _Sens2;
     uint16_t _C[N_PROM_PARAMS];
     uint32_t _lastTime;
+
+    i2c_dt_spec *dev;
 };
 
 #endif
